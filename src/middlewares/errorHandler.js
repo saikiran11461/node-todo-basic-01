b19061ace9c53f9e949c3c203b1993becf7c8252
saikiran11461 <saikiran11471@gmail.com>
2025-05-18 09:50:20 -0700

    addednewchanges

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,5 +1,5 @@
 // errorHandler.js
 exports.errorHandler = (err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ error: 'Something went wrong!' });
+  res.status(500).json({ error: 'Something went wrong!!' });
 };
